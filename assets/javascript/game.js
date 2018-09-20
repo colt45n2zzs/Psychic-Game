43e783fac5768148ea10f53636f0ddd9c1892f5a
Colton <coltonjohnson@gmail.com>
2018-09-20 12:15:09 -0400

    update 092018-1215

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,69 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
- 
-
-
-        document.onkeyup = function(event) {
-        userGuess.textContent = event.key;
-        computerGuess.textContent = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        var userGuess = event.key;
-        var winTotal = winGame.textContent;
-        var loseTotal = loseGame.textContent;
-        var guessRemainTotal = guessRemain.textContent;
-        var guessMade = guessMade.textContent;
-        var guessMadeArr = []; 
-
-        if (userGuess != computerGuess.textContent) {
-            guessRemainTotal--;
-            guessRemain.innerHTML = guessRemainTotal;
+var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var choices2 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesRemain = 9;
+var guesses = [];
+var computerGuesses = [];
+guessesRemainGame.textContent = guessesRemain;
+winsGame.textContent = wins;
+lossesGame.textContent = losses;
 
 
 
 
 
-        }
+document.onload = function() {
+   var compGuess = choices[Math.floor(Math.random() * choices.length)];
+   computerGuesses.push(compGuess);
+   console.log(computerGuesses[0]);
+
+   
+};
+
+document.onkeyup = function(event) {
+    var playerGuess = event.key;
+    guessesGame.textContent = playerGuess;
+    guesses.push(playerGuess);
 
 
 
 
 
+if (playerGuess == computerGuesses[0] && guessesRemain > 0) {
+    wins++;
+    winsGame.textContent = wins;
+    guessesRemain = 9;
+    guessesRemainGame.textContent = guessesRemain;
+    guesses.length = 0;
+    guessesGame.textContent = "";
+    computerGuesses.length = 0;
+    var compGuess = choices[Math.floor(Math.random() * choices.length)];
+    computerGuesses.push(compGuess);
+    console.log(computerGuesses[0]);
+    
+    
+}
+
+else if (playerGuess != computerGuesses[0] && guessesRemain > 0) {
+    guessesRemain = guessesRemain - 1;
+    guessesRemainGame.textContent = guessesRemain;
+    guessesGame.textContent = guesses.join("  ");    
+   
+}
 
 
+else {
+    losses++;
+    lossesGame.textContent = losses;
+    guessesRemain = 9;
+    guessesRemainGame.textContent = guessesRemain;
+    guesses.length = 0;
+    guessesGame.textContent = "";
+    computerGuesses.length = 0;
+    var compGuess = choices[Math.floor(Math.random() * choices.length)];
+    computerGuesses.push(compGuess);
+    console.log(computerGuesses[0]);
 
-
-        
-
-        }
+    }
+};
